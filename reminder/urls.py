b849d221bb84ef374d44faef9853a9fc3cf44bdd
Yujin <yujinjcho@gmail.com>
2016-09-19 12:45:49 -0700

    Created outline of page with birthday reminders
    Primarily using Bootstrap. Currently responsive as well.

--- reminder/urls.py
+++ reminder/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'reminder'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^birthdays/$', views.birthdays, name='birthdays'),
 ]
--- reminder/views.py
+++ reminder/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'reminder/index.html')
+
+def birthdays(request):
+    return render(request, 'reminder/birthdays.html')
