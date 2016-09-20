3e1fb12a938cfe708a16dbb69b206b6cd01b3f52
Yujin <yujinjcho@gmail.com>
2016-09-20 14:09:40 -0700

    Move JS functions into anonymous self-executing function
    Replaced onclick calls to Addeventlistners.

--- reminder/static/reminder/js/manage_groups.js
+++ reminder/static/reminder/js/manage_groups.js
@@ -3,8 +3,58 @@
   var upcomingButton = document.getElementById("upcoming-btn");
   var pastButton = document.getElementById("past-btn");
 
-  upcomingButton.addEventListener("click", toggleButtons);
-  pastButton.addEventListener("click", toggleButtons);
+  function greetingBtnListen(){
+    var arrayLength = patient_data.length;
+    for (var i = 0; i < arrayLength; i++) {
+      var greetBtnElem = document.getElementById('greet-'.concat(patient_data[i].id));
+      greetBtnElem.addEventListener("click", toggleCheckMark)
+    }
+  }  
+
+  function toggleCheckMark(e) {
+      var children = document.getElementById(e.currentTarget.id.substr(6)).childNodes;
+
+      for (var i = 0; i < children.length; i++) {
+          if (children[i].tagName == "SPAN") {
+              var checkSign = children[i];
+              if (checkSign.className.includes('msg-sent')) {
+                  return
+              }
+              else {
+                  checkSign.className = checkSign.className.concat(' msg-sent')
+              }
+          }
+      }
+  }
+
+  function patientListListen(){
+    var arrayLength = patient_data.length;
+    for (var i = 0; i < arrayLength; i++) {
+      var patientElem = document.getElementById(patient_data[i].id);
+      patientElem.addEventListener("click", showProfile)
+    }
+  }
+
+  function showProfile(e){
+      debugger;
+      hideProfiles();
+      var arrayLength = patient_data.length;
+      for (var i = 0; i < arrayLength; i++) {
+          if (patient_data[i].id == e.currentTarget.id ) {
+            
+            var activeProfile = document.getElementById('profile-'.concat(patient_data[i].id));
+            activeProfile.className = "patient-profile";
+          };
+      };
+    };
+
+  function hideProfiles(){
+    var profiles = document.getElementsByClassName("patient-profile");
+    var arrayLength = profiles.length;
+    for (var i = 0; i < arrayLength; i++) {
+        profiles[i].className = "patient-profile profile-hidden";
+    };
+  };
 
   function toggleButtons(e){
     if (e.target.className == 'btn btn-default active') {
@@ -38,10 +88,15 @@
     };
   };
 
-  
+  function main(){
+    patientListListen();
+    upcomingButton.addEventListener("click", toggleButtons);
+    pastButton.addEventListener("click", toggleButtons);
+    greetingBtnListen()
+  }
 
-  
 
+  main()
 })();
 
 
