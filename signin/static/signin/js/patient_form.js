d4ca8b854c48d6dc2022d0a5e0ccb130c37b5de4
Yujin <yujinjcho@gmail.com>
2016-09-25 18:04:55 -0700

    Remove deletes associated with patient_form_submit
    Previously included all attributes from patient object because
    PUT request was being used with drchrono API. This required
    deleting some attributes because would cause an error.
    
    Switching to use a PATCH request now resulting in no need for
    these deletes.

--- signin/static/signin/js/patient_form.js
+++ signin/static/signin/js/patient_form.js
@@ -9,9 +9,9 @@
               if (cookie.substring(0, name.length + 1) == (name + '=')) {
                   cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                   break;
-              }
-          }
-      }
+              };
+          };
+      };
       return cookieValue;
   };
 
@@ -19,59 +19,38 @@
   function submitListen(){
     var submitBtn = document.getElementById('form-submit');
     submitBtn.addEventListener('click', submitForm);
-  }
+  };
 
   //get values for each input
   function submitForm(){
     var csrftoken = getCookie('csrftoken');
     
-    patient_data['csrfmiddlewaretoken'] = csrftoken;
-    patient_data['doctor'] = doctor_id;
-    patient_data['patient_id'] = patient_id;
-
-    // I was originally using a PUT request instead of PATCH for
-    // the API. Did't want to lose information, so kept all fields,
-    // which was causing an error until I removed the right field.
-    delete patient_data['updated_at']
-    delete patient_data['offices']
-    delete patient_data['id']
-    delete patient_data['primary_care_physician']
-    delete patient_data['date_of_first_appointment']
-    delete patient_data['ethnicity']
-    delete patient_data['disable_sms_messages']
-    delete patient_data['patient_photo_date']
-    delete patient_data['patient_photo']
-    delete patient_data['date_of_last_appointment']
-    delete patient_data['social_security_number']
-    delete patient_data['chart_id']
-    delete patient_data['preferred_language']
-    delete patient_data['race']
-    delete patient_data['copay']
-    ////
-  
+    var postData = {
+      'csrfmiddlewaretoken': csrftoken,
+      'doctor': doctor_id,
+      'patient_id': patient_id  
+    };
+    
     var arrayLength = data.length;
     for (var i = 0; i < arrayLength; i++){
       var currentField = document.getElementById("patient-".concat(data[i]));
-      patient_data[data[i]] = currentField.value;
-    }
+      postData[data[i]] = currentField.value;
+    };
 
     $.ajax({
       type: "POST",
       url: "http://127.0.0.1:8000/signin/patient_form_submit/",
-      data: patient_data,
+      data: postData,
       success: function(data){window.location = exit_url},
       error: function(XMLHttpRequst, textStatus, errorThrown){
         alert("There are some errors with your input, please fix them.")
-      }
+      };
     });
-
   };
 
   function main() {
-    submitListen()
-  }
+    submitListen();
+  };
 
   main();
-
-
 })();
