c7f67ca847ed86ebd505df2a37cec74622c9fdfc
Yujin <yujinjcho@gmail.com>
2016-10-24 18:31:33 -0700

    Remove datetime-to-string conversion test

--- signin/tests.py
+++ signin/tests.py
@@ -2,14 +2,6 @@ from django.test import TestCase
 
 from . import views
 
-class ConvertDate(TestCase):
-
-    def test_convert_time_to_str(self):
-        self.assertEqual(views.convert_time_to_str('02:30:00'),'2:30AM')
-        self.assertEqual(views.convert_time_to_str('12:30:00'),'12:30PM')
-        self.assertEqual(views.convert_time_to_str('13:30:00'),'1:30PM')
-        self.assertEqual(views.convert_time_to_str('24:30:00'),'12:30AM')
-        
 
 class TestPhoneRegex(TestCase):
 
