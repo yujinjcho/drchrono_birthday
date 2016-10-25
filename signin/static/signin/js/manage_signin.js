6e9dacc6189f066e91e68ec3ad4c44ef6b1bf867
Yujin <yujinjcho@gmail.com>
2016-10-25 10:32:50 -0700

    Make manage_signin js-google style compliant and refactored code
    Instead of manually creating elements with text, used document
    methods. Also, create a general method for returning
    and element for some of the identifying attributes that are
    returned on that patient_signin.

--- signin/static/signin/js/manage_signin.js
+++ signin/static/signin/js/manage_signin.js
@@ -1,97 +1,108 @@
-(function (){
+(function() {
 
   function getCookie(name) {
-      var cookieValue = null;
-      if (document.cookie && document.cookie != '') {
-          var cookies = document.cookie.split(';');
-          for (var i = 0; i < cookies.length; i++) {
-              var cookie = jQuery.trim(cookies[i]);
-              if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                  break;
-              }
-          }
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = jQuery.trim(cookies[i]);
+        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
       }
-      return cookieValue;
+    }
+    return cookieValue;
   };
 
-  function handlePatientResponse(data){
-    var patientFound = document.getElementById("patient-found-alert")
-    var patientNotFound = document.getElementById("patient-not-found-alert")
+  function handlePatientResponse(data) {
+    var patientFound = document.getElementById('patient-found-alert');
+    var patientNotFound = document.getElementById('patient-not-found-alert');
 
     if (data == null) {
-      //If Patient information is NOT found
-      patientNotFound.className = "alert alert-warning"
-      patientFound.className = "alert alert-success patient-alert"
-    }
-    else {
-      //If Patient information is found
-      patientFound.className = "alert"
-      patientNotFound.className = "alert alert-warning patient-alert"
+      //If Patient information is not found
+      patientNotFound.className = 'alert alert-warning';
+      patientFound.className = 'alert alert-success patient-alert';
+    } else {
+      patientFound.className = 'alert';
+      patientNotFound.className = 'alert alert-warning patient-alert';
       createPatientInfo(data);
       setAppointmentFinder(data);
     }
   };
 
-  function createPatientInfo(data){
-    var patientInfo = document.getElementById("patient-info");
-    patientInfo.innerHTML = "<div id='profile-pic'></div><div id='social-security-number'></div><div id='email'></div>";
-    var profilePic = document.getElementById("profile-pic");
-    var socialSecurityNumber = document.getElementById('social-security-number');
-    var email = document.getElementById('email');
+  function createPatientInfo(data) {
+    var patientInfo = document.getElementById('patient-info');
+    patientInfo.innerHTML = '';
 
-    profilePic.innerHTML = '<img class="profile-pic" src='.concat(data.patient_photo).concat('>')
-    socialSecurityNumber.innerHTML = '<strong>SSN</strong>: '.concat(data.social_security_number);
-    email.innerHTML = '<strong>Email</strong>: '.concat(data.email);
+    var profilePic = document.createElement('DIV');
+    profilePic.setAttribute('id', 'profile-pic');
+    var profileImage = document.createElement('IMG');
+    profileImage.setAttribute('class', 'profile-pic');
+    profileImage.setAttribute('src', data.patient_photo);
+    profilePic.appendChild(profileImage);
+
+    patientInfo.appendChild(profilePic);
+    patientInfo.appendChild(
+      createPatientIdentifier('SSN', data.social_security_number)
+    );
+    patientInfo.appendChild(createPatientIdentifier('Email', data.email));
   };
 
-  function setAppointmentFinder(data){
+  function createPatientIdentifier(IdType, IdValue) {
+    var elem = document.createElement('DIV');
+    elem.setAttribute('id', IdType.toLowerCase());
+    var elemText = document.createElement('STRONG');
+    elemText.innerHTML = IdType.concat(': ').concat(IdValue);
+    elem.appendChild(elemText);
+    return elem;
+  }
+
+  function setAppointmentFinder(data) {
     var appointmentBtn = document.getElementById('check-appointment-btn');
     var appointmentHref = appointmentBtn.getAttribute('href');
     var patientId = data.id;
 
-    if (appointmentHref.substr(-1) == "/") {
+    if (appointmentHref.substr(-1) == '/') {
       appointmentBtn.setAttribute(
         'href',
         appointmentHref.concat('?id=').concat(patientId)
       );
     } else {
       if (appointmentHref.includes(patientId)) {
-        return
+        return;
       } else {
         appointmentBtn.setAttribute(
-        'href',
-        appointmentHref.substring(0, 27).concat('?id=').concat(patientId)
-      );
+          'href',
+          appointmentHref.substring(0, 27).concat('?id=').concat(patientId)
+        );
       }
     }
-
-
   };
 
   function findPatient() {
 
-    var firstName = document.getElementById("InputFirstName")
-    var lastName =document.getElementById("InputLastName")
-    var dateOfBirth = document.getElementById("DateofBirth")
+    var firstName = document.getElementById('InputFirstName');
+    var lastName = document.getElementById('InputLastName');
+    var dateOfBirth = document.getElementById('DateofBirth');
     var csrftoken = getCookie('csrftoken');
 
     $.post(
-      "http://127.0.0.1:8000/signin/find_patient/",
+      'http://127.0.0.1:8000/signin/find_patient/',
       {
-        "first_name": firstName.value,
-        "last_name": lastName.value,
-        "date_of_birth": dateOfBirth.value,
+        'first_name': firstName.value,
+        'last_name': lastName.value,
+        'date_of_birth': dateOfBirth.value,
         'csrfmiddlewaretoken': csrftoken
       },
-      function(data){handlePatientResponse(data)}
+      function(data) {handlePatientResponse(data);}
     );
 
   };
 
-  function main(){
+  function main() {
     var patientSigninBtn = document.getElementById('patient-signin-btn');
-    patientSigninBtn.addEventListener('click',findPatient)
+    patientSigninBtn.addEventListener('click', findPatient);
   };
 
   main();
