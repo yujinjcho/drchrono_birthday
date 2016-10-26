15f4c60b66c239686ef7537e50a7265b183b1593
Yujin <yujinjcho@gmail.com>
2016-10-25 17:27:06 -0700

    Make patient_form.js jscs google compliant

--- signin/static/signin/js/patient_form.js
+++ signin/static/signin/js/patient_form.js
@@ -1,49 +1,53 @@
-(function (){
+(function() {
 
   function getCookie(name) {
-      var cookieValue = null;
-      if (document.cookie && document.cookie != '') {
-          var cookies = document.cookie.split(';');
-          for (var i = 0; i < cookies.length; i++) {
-              var cookie = jQuery.trim(cookies[i]);
-              if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                  break;
-              };
-          };
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = jQuery.trim(cookies[i]);
+        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        };
       };
-      return cookieValue;
+    };
+    return cookieValue;
   };
 
   //add event listener on submit button
-  function submitListen(){
+  function submitListen() {
     var submitBtn = document.getElementById('form-submit');
     submitBtn.addEventListener('click', submitForm);
   };
 
   //get values for each input
-  function submitForm(){
+  function submitForm() {
     var csrftoken = getCookie('csrftoken');
 
     var postData = {
       'csrfmiddlewaretoken': csrftoken,
-      'doctor': doctor_id,
-      'patient_id': patient_id
+      'doctor': doctorId,
+      'patient_id': patientId
     };
 
     var arrayLength = data.length;
-    for (var i = 0; i < arrayLength; i++){
-      var currentField = document.getElementById("patient-".concat(data[i]));
+    for (var i = 0; i < arrayLength; i++) {
+      var currentField = document.getElementById('patient-'.concat(data[i]));
       postData[data[i]] = currentField.value;
     };
 
+    var postUrl = document.createElement('A');
+    postUrl.href = window.location.href;
+    postUrl.pathname = '/signin/patient_form_submit/';
+
     $.ajax({
-      type: "POST",
-      url: "http://127.0.0.1:8000/signin/patient_form_submit/",
+      type: 'POST',
+      url: postUrl.href,
       data: postData,
-      success: function(data){window.location = exit_url},
-      error: function(XMLHttpRequst, textStatus, errorThrown){
-        alert("There are some errors with your input, please fix them.")
+      success: function(data) { document.patientForm.submit(); },
+      error: function(XMLHttpRequst, textStatus, errorThrown) {
+        alert('There are some errors with your input, please fix them.');
       }
     });
   };
