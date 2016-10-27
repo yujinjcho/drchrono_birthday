f8a2e8e705df25f3171e48d0dee2264fd931560f
Yujin <yujinjcho@gmail.com>
2016-10-26 17:45:04 -0700

    Define profile model for user id

--- signin/models.py
+++ signin/models.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    drchrono_id = models.CharField(max_length=100)
